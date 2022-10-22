9d56afbc13a4d896fabfabbaf35b9ab2bebfbf74
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 16:11:20 -0400

    Update 4_Self_Examination.py

--- pages/4_Self_Examination.py
+++ pages/4_Self_Examination.py
@@ -1,6 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title="Self Examination")
-st.sidebar.header("Self Examination")
 st.title("Self Examination")
 
 st.write("Written Tutorial: https://www.breastcancer.org/screening-testing/breast-self-exam-bse")
