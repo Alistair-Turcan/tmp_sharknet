e89c0bf332685d606b0c3a9dc2ef89bd06023d98
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 15:16:35 -0400

    Create 3_General_Resources.py

---
+++ pages/3_General_Resources.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="General Resources")
+st.sidebar.header("General Resources")
+st.title("General Resources")
+
+st.write("Resources: https://www.nationalbreastcancer.org/about-breast-cancer/early-detection/breast-cancer-resources")
+st.write("Facts: https://www.nationalbreastcancer.org/breast-cancer-facts")
+st.write("Covering the Cost of Care: https://www.breastcancer.org/managing-life/covering-cost-of-care")
