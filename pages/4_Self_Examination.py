933cd6f2a09331fa6fd9fcf84b6a2700c0ae14bd
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 16:14:58 -0400

    Update 4_Self_Examination.py

--- pages/4_Self_Examination.py
+++ pages/4_Self_Examination.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="Self Examination")
 st.title("Self Examination")
 
-st.write("Written Tutorial: https://www.breastcancer.org/screening-testing/breast-self-exam-bse")
+st.write("# Written Tutorial")
+st.write("https://www.breastcancer.org/screening-testing/breast-self-exam-bse")
 
-st.write("Video Tutorial")
+st.write("# Video Tutorial")
 st.video('https://www.youtube.com/watch?v=LrfE6JUwIms')
