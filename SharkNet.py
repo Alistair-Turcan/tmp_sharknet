7e19373a1793f3299ef2dcc2000067a854df2be3
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 16:02:17 -0400

    Update SharkNet.py

--- SharkNet.py
+++ SharkNet.py
@@ -3,4 +3,6 @@ st.title("SharkNet: Automatic Breast Cancer Classification from Thermography")
 
 st.write("SharkNet is a system for predicting and interpreting potential breast cancer from thermographies. It is designed to assist both primary caregivers and patients discuss if further testing such as mammography is needed.")
 
+st.write("Click on Thermograph Processing to get started.")
 
+st.write("Additional resources for breast cancer awareness and management are also made available through the General Resources and Self Examination tabs on the left.")
