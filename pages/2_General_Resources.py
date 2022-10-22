45367330e3d325afcef1815bf3cef42793a56874
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 15:09:03 -0400

    Create 2_General_Resources.py

---
+++ pages/2_General_Resources.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title="Self Examination")
+st.sidebar.header("Self Examination")
+st.title("Self Examination")
+
+st.write("Written Tutorial: https://www.breastcancer.org/screening-testing/breast-self-exam-bse")
+
+st.write("Video Tutorial")
+st.video('https://www.youtube.com/watch?v=LrfE6JUwIms')
